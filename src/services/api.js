91674ea57b61a8b9feeb6bc0d0d59486a7f654a9
afrutheenn <afrudeenja@gmail.com>
2025-05-05 18:55:23 +0530

    live

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', 
+  baseURL: 
+  // 'http://localhost:5000/api', 
+  'https://vertiflixapi.whydev.in/api',
   headers: {
     'Content-Type': 'application/json',
   },
